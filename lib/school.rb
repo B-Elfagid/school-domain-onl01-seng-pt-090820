7828bb5338a11e0358f8fe629fded46d7c89732a
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-26 20:40:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,6 +20,16 @@ class School
   end 
 
 
+
+
+def sort  #part 4
+    sorted = {} 
+    roster.each do |grade, students| #iterate through each grade and student
+      sorted[grade] = students.sort #grade is the key in the sorted hash. Value is the students sorted in ABC order.
+    end
+    sorted #the new sorted roster.
+  end
+end
 def sort 
   nu_hash = {}
   roster.each do |x, y| 
